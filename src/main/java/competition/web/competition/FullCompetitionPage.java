e63dfa8c5d5142c214832ebbedf0578ab2909fc6
Mihai <dpmihai@yahoo.com>
2015-08-31 12:07:58 +0300

    test for PageParameter

--- src/main/java/competition/web/competition/FullCompetitionPage.java
+++ src/main/java/competition/web/competition/FullCompetitionPage.java
@@ -5,12 +5,15 @@ import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.apache.wicket.util.string.StringValue;
+import org.apache.wicket.util.string.StringValueConversionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import competition.domain.entity.Competition;
 import competition.service.GeneralService;
 import competition.web.BaseTemplatePage;
+import competition.web.security.LoginPage;
 
 public class FullCompetitionPage extends BaseTemplatePage {
 	
@@ -28,14 +31,24 @@ public class FullCompetitionPage extends BaseTemplatePage {
 		super(pageParameters);
 		setOutputMarkupId(true);			
 		
-		competitionId = pageParameters.get("competitionId").toInt();				
-		Competition competition = generalService.find(Competition.class, competitionId);	
+		competitionId = pageParameters.get("competitionId").toInt();	
 		
-		boolean top = pageParameters.get("top").toBoolean();
-		
-		panel = new BaseCompetitionPanel("basePanel", new Model<Competition>(competition), top);		
-		add(panel);
-		
+		StringValue sci = pageParameters.get("competitionId");
+		try {
+			competitionId = sci.toInt();
+			
+			Competition competition = generalService.find(Competition.class, competitionId);	
+			
+			boolean top = pageParameters.get("top").toBoolean();
+			
+			panel = new BaseCompetitionPanel("basePanel", new Model<Competition>(competition), top);		
+			add(panel);
+			
+		} catch (StringValueConversionException ex) {
+			LOG.error(ex.getMessage());
+			getRequestCycle().setResponsePage(LoginPage.class);
+		} 		
+				
 	}	
 	
 	public void refresh(AjaxRequestTarget target) {
