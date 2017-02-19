23f292f69b23327bca65117b222afe68729e6c16
dpmihai <dpmihai@yahoo.com>
2017-02-19 17:21:19 +0200

    wicket utilities

---
+++ src/main/java/competition/web/util/WicketUtil.java
@@ -0,0 +1,26 @@
+package competition.web.util;
+
+import org.apache.wicket.Session;
+import org.apache.wicket.core.request.ClientInfo;
+import org.apache.wicket.protocol.http.ClientProperties;
+import org.apache.wicket.protocol.http.request.WebClientInfo;
+
+public class WicketUtil {
+	
+	public static boolean isSmallScreen() {
+		boolean smallScreen = false;
+		int width = -1;
+		ClientInfo info = Session.get().getClientInfo();
+		if (info instanceof WebClientInfo) {
+		   	ClientProperties cp = ((WebClientInfo) info).getProperties();
+		   	width = cp.getBrowserWidth();
+		}
+			
+		// tablet & phone
+		if ((width > 0) && (width <= 800)) {
+			smallScreen = true;
+		}
+		return smallScreen;
+	}
+
+}
